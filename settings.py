9ff3704ea88d08cb59112649fa783396b361fd69
Daniel Solarin <soladan2000@gmail.com>
2025-01-15 10:13:03 -0900

    Add UID settings for Binance, MEXC, and Bybit in settings.py

--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ load_dotenv(override=True)
 
 
 class Settings:
+    BINANCE_UID = int(os.getenv("binance_uid"))
+    MEXC_UID = int(os.getenv("bybit_uid"))
+    BYBIT_UID = int(os.getenv("mexc_uid"))
     DB_PATH = os.getenv("DB_PATH")
     
     

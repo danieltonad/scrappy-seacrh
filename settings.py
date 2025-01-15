7637fb6361e85f8f91020ca1e8579705486a1669
Daniel Solarin <soladan2000@gmail.com>
2025-01-15 10:15:51 -0900

    Remove unnecessary blank lines in settings.py

--- settings.py
+++ settings.py
@@ -9,6 +9,5 @@ class Settings:
     MEXC_UID = int(os.getenv("bybit_uid"))
     BYBIT_UID = int(os.getenv("mexc_uid"))
     DB_PATH = os.getenv("DB_PATH")
-    
-    
+        
 settings = Settings()

d7f681bc1cf2336de3028cef71f1112fbbbf5cb5
Daniel Solarin <soladan2000@gmail.com>
2025-01-16 13:38:45 -0900

    Update main function to generate wallet addresses from a predefined seed and display them by wallet type

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from wallets import generate_wallet_address, generate_public_key
 import asyncio
 
 
+
+
 async def main():
-    for i,seed in enumerate(await spawn_seed()):
-        if is_valid_phrase(" ".join(seed)):
-            print(f"{i:,}", end="\r")
+    # for i,seed in enumerate(await spawn_seed()):
+    #     if is_valid_phrase(" ".join(seed)):
+    #         print(f"{i:,}", end="\r")
+    
+    seed = "fatigue purpose cable sniff tool lock roast risk pipe hunt buyer illness"
+    for wallet in WalletType.get_wallet_types():
+        wallet_address = await generate_wallet_address(seed, WalletType.coin_seedler(wallet)[1], WalletType.coin_seedler(wallet)[0])
+        print(f"{wallet}:", wallet_address)
             
             
 asyncio.run(main())

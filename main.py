ce5d55ba197d9b3ad87df9d3a6e127146598798c
Daniel Solarin <soladan2000@gmail.com>
2025-01-19 11:12:50 -0900

    Add asynchronous seed scanning for wallet addresses and exchanges

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from wallet_type_enum import WalletType
+from exchange_enum import Exchange
 from seedler import spawn_seed, is_valid_phrase
 from wallets import generate_wallet_address, generate_public_key
 import asyncio, json
 
 feeder = json.load(open("feeder.json", "r"))
 
-print(feeder)
+async def scan_seed(seed: str):
+    for exchange in feeder.keys():
+        itr = Exchange.get_exchange_uid(Exchange[exchange])
+        exchange_dict = feeder[exchange]
+        for wallet in WalletType.get_wallet_types():
+            seedler, coin = WalletType.coin_seedler(wallet)
+            wallet_address: str = await generate_wallet_address(seed, coin, seedler, itr)
+            if wallet_address.lower() == exchange_dict.get(wallet.value).lower():
+                print(f"{wallet.value}:", wallet_address.lower(), exchange_dict.get(wallet.value).lower(), exchange)
+            
+            print(f"{wallet.value}:", wallet_address.lower(), exchange_dict.get(wallet.value).lower(), exchange)
 
 
 
@@ -16,10 +27,11 @@ async def main():
     #         print(f"{i:,}", end="\r")
     
     seed = "fatigue purpose cable sniff tool lock roast risk pipe hunt buyer illness"
-    for wallet in WalletType.get_wallet_types():
-        seedler, coin = WalletType.coin_seedler(wallet)
-        wallet_address = await generate_wallet_address(seed, coin, seedler)
-        print(f"{wallet}:", wallet_address)
+    await scan_seed(seed)
+    # for wallet in WalletType.get_wallet_types():
+    #     seedler, coin = WalletType.coin_seedler(wallet)
+    #     wallet_address = await generate_wallet_address(seed, coin, seedler)
+    #     print(f"{wallet}:", wallet_address)
             
             
 asyncio.run(main())

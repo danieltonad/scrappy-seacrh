35dbdeef3de8a2816c6ce9964bb024a5fb9ebbfa
Daniel Solarin <soladan2000@gmail.com>
2025-01-19 10:17:21 -0900

    Refactor wallet address generation and update coin seedler logic for BCH and LTC

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ async def main():
     
     seed = "fatigue purpose cable sniff tool lock roast risk pipe hunt buyer illness"
     for wallet in WalletType.get_wallet_types():
-        wallet_address = await generate_wallet_address(seed, WalletType.coin_seedler(wallet)[1], WalletType.coin_seedler(wallet)[0])
+        seedler, coin = WalletType.coin_seedler(wallet)
+        wallet_address = await generate_wallet_address(seed, coin, seedler)
         print(f"{wallet}:", wallet_address)
             
             
--- wallet_type_enum.py
+++ wallet_type_enum.py
@@ -16,13 +16,13 @@ class WalletType(Enum):
     
     @staticmethod
     def coin_seedler(wallet_type: 'WalletType') -> str:
-        from bip_utils import Bip84, Bip84Coins,  Bip44, Bip44Coins
+        from bip_utils import Bip84, Bip84Coins,  Bip44, Bip44Coins, Bip49, Bip49Coins
         wallets = {
             WalletType.BTC:(Bip84, Bip84Coins.BITCOIN),
             WalletType.BNB: (Bip44, Bip44Coins.BINANCE_SMART_CHAIN),
             WalletType.TRX:(Bip44, Bip44Coins.TRON),
-            WalletType.BCH:(Bip44, Bip44Coins.BITCOIN_CASH),
-            WalletType.LTC:(Bip44, Bip44Coins.LITECOIN),
+            WalletType.BCH:(Bip49, Bip49Coins.BITCOIN_CASH),
+            WalletType.LTC:(Bip84, Bip84Coins.LITECOIN),
             WalletType.XRP:(Bip44, Bip44Coins.RIPPLE),
             WalletType.SOL:(Bip44, Bip44Coins.SOLANA),
             WalletType.DOGE:(Bip44, Bip44Coins.DOGECOIN),

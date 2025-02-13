f2a11ff877004ab2964bdb3f5e4a5d0d20434e3c
Daniel Solarin <soladan2000@gmail.com>
2025-02-13 03:33:31 -0900

    Add TRX holders fetching and saving functionality; implement private key generation and address conversion

--- pkey-trx/main.py
+++ pkey-trx/main.py
@@ -1,10 +1,31 @@
 from tronpy.keys import PrivateKey
+import random, secrets, sha3, ecdsa
 
-# Example Private Key (replace with your own)
-private_key_hex = "ef8c3f61db859e035808fe037999c3554f4ae4ef5ba4ac9a095db6e0b6fe5cdf"
+def generate_trx_private_key() -> tuple:
+    return secrets.token_hex(32), ''.join(random.choices('0123456789abcdef', k=64))
 
-# Generate Public Key & Address
-priv_key = PrivateKey(bytes.fromhex(private_key_hex))
-trx_address = priv_key.public_key.to_base58check_address()
 
-print("TRX Address:", trx_address)
+def trx_private_key_to_address(private_key_hex: str) -> str:
+    priv_key = PrivateKey(bytes.fromhex(private_key_hex))
+    return priv_key.public_key.to_base58check_address()
+
+def eth_private_key_to_address(private_key_hex: str) -> str:
+    private_key_bytes = bytes.fromhex(private_key_hex)
+    sk = ecdsa.SigningKey.from_string(private_key_bytes, curve=ecdsa.SECP256k1)
+    vk = sk.verifying_key
+    public_key = b'\x04' + vk.to_string()
+    keccak = sha3.keccak_256()
+    keccak.update(public_key[1:])
+    eth_address = keccak.digest()[-20:]
+    return '0x' + eth_address.hex()
+
+# private_key_hex = "5aed2a12b51d621700f719c95115994cbc250347cab075e5e745d2eb0c33986a"
+pk1, pk2 = generate_trx_private_key()
+
+print("Private Key:", pk1)
+print("TRX Address:", trx_private_key_to_address(pk1))
+print("ETH Address:", eth_private_key_to_address(pk1))
+
+print("Private Key:", pk2)
+print("TRX Address:", trx_private_key_to_address(pk2))
+print("ETH Address:", eth_private_key_to_address(pk2))
---
+++ scrapper/trx_holders.py
@@ -0,0 +1,40 @@
+from httpx import AsyncClient
+import asyncio, os
+
+def save_trx_holders(holders: list):
+    trx_path = "../data/trx_holders.txt"
+    prev_holders = []
+    if os.path.exists(trx_path):
+        with open(trx_path, "r") as f:
+            prev_holders = f.read().splitlines()
+    holders = list(set(holders + prev_holders))
+    with open(trx_path, "w") as f:
+        for holder in holders:
+            f.write(f"{holder}\n")
+            
+async def fetch_trx_holders(start: int, limit: int = 50):
+    url = f"https://apilist.tronscanapi.com/api/account/list?sort=&limit={limit}&start={start}"
+    async with AsyncClient() as client:
+        response = await client.get(url)
+        data = response.json().get("data", [])    
+        return [dt["address"] for dt in data] if len(data) > 0 else []
+
+async def get_trx_holders():
+    limit = 50
+    all_data = []
+    # url.format(20, 10)
+    while True:
+        start = len(all_data)
+        all_data += await fetch_trx_holders(start, limit)
+        
+        if len(all_data) > 9_990:
+            break
+        
+        print(f"Progress: {len(all_data)} ...", end="\r")
+        await asyncio.sleep(1)
+    
+    
+    return all_data
+
+res = asyncio.run(get_trx_holders())
+save_trx_holders(res)

ef8e908ef3a21e730df28424300ebe3f748c1f73
Daniel Solarin <soladan2000@gmail.com>
2025-02-14 05:47:46 -0900

    Refactor Ethereum address generation to use hashlib for keccak_256 and clean up commented code

--- pkey/main.py
+++ pkey/main.py
@@ -1,5 +1,5 @@
 from tronpy.keys import PrivateKey
-import random, secrets, sha3, ecdsa
+import random, secrets, hashlib, ecdsa
 
 
 trx_path = "../data/trx_holders.txt"
@@ -26,12 +26,13 @@ def trx_private_key_to_address(private_key_hex: str) -> str:
     priv_key = PrivateKey(bytes.fromhex(private_key_hex))
     return priv_key.public_key.to_base58check_address()
 
+
 def eth_private_key_to_address(private_key_hex: str) -> str:
     private_key_bytes = bytes.fromhex(private_key_hex)
     sk = ecdsa.SigningKey.from_string(private_key_bytes, curve=ecdsa.SECP256k1)
     vk = sk.verifying_key
     public_key = b'\x04' + vk.to_string()
-    keccak = sha3.keccak_256()
+    keccak = hashlib.sha3_256()
     keccak.update(public_key[1:])
     eth_address = keccak.digest()[-20:]
     return '0x' + eth_address.hex()
@@ -73,7 +74,6 @@ def main():
 main()
 
 # pk1, pk2 = generate_private_key()
-# private_key_hex = "5aed2a12b51d621700f719c95115994cbc250347cab075e5e745d2eb0c33986a"
 # print("Private Key:", pk1)
 # print("TRX Address:", trx_private_key_to_address(pk1))
 # print("ETH Address:", eth_private_key_to_address(pk1))
